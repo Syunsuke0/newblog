48cab9dd22256fa2934db9df76f4e98dba4010ba
Syunsuke Shima <s.syun617@icloud.com>
2025-03-21 11:55:40 +0900

    🎉

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { HeaderSimple } from "./components/Header/HeaderSimple";
+
+export default function Home() {
+  return (
+    <div>
+      <HeaderSimple />
+    </div>
+  );
+}
