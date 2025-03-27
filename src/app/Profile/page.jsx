129fb3f7ced60f69de2e59acc431f9783840187d
Syunsuke Shima <s.syun617@icloud.com>
2025-03-27 19:24:01 +0900

    📝 プロフィール文と画像を追加

--- src/app/Profile/page.jsx
+++ src/app/Profile/page.jsx
@@ -1,7 +1,24 @@
 const ProfilePage = () => {
   return (
     <div>
-      <h1 className="text-4xl">プロフィールページ</h1>
+
+      <Image
+        src="/user.png"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+        className="rounded-full m-auto"
+      />
+
+      <h2 className="flex justify-center underline">sh-ke - プロフィール</h2>
+      <p className="px-60">
+        ガジェット、音楽、アニメが好き。
+        <br />
+        趣味はライブ参加とベースを弾くこと。
+        弾いてみた動画をあげることが今の目標。
+        <br />
+        好きなアーティストはAooo。
+      </p>
     </div>
   );
 };
