ef42aeefe9a34fa4aa288b98679d7c48964ee393
Syunsuke Shima <s.syun617@icloud.com>
2025-04-03 15:29:07 +0900

    🚸 リロード時のナビゲーションの状態管理を修正

--- src/app/components/Header/HeaderSimple.jsx
+++ src/app/components/Header/HeaderSimple.jsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useState } from "react";
 import { Burger, Container, Group, Anchor } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { MantineLogo } from "@mantinex/mantine-logo";
 import classes from "./HeaderSimple.module.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const links = [
   { link: "/", label: "Home" },
@@ -16,17 +16,14 @@ const links = [
 
 const HeaderSimple = () => {
   const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
+  const pathname = usePathname();
 
   const items = links.map((link) => (
     <Link
       key={link.label}
       href={link.link}
       className={classes.link}
-      data-active={active === link.link || undefined}
-      onClick={(event) => {
-        setActive(link.link);
-      }}
+      data-active={pathname === link.link || undefined}
     >
       {link.label}
     </Link>
