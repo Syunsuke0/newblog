34b49c41ccb791d6e8f81c0af98e520e87b6e250
Syunsuke Shima <s.syun617@icloud.com>
2025-09-24 10:48:24 +0900

    ✨ 404ページの追加

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <main>
+      <h1 className="text-center text-8xl pt-20 pb-10 font-extrabold">404</h1>
+      <p className="text-center text-3xl font-extrabold">
+        ページが見つかりませんでした
+      </p>
+    </main>
+  );
+};
+
+export default NotFound;
